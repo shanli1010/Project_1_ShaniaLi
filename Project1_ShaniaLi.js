45b06f51c8047a1354ae8f596097520f1e1d3a15
shanli1010 <90589845+shanli1010@users.noreply.github.com>
2021-10-27 14:08:38 -0400

    Add files via upload

---
+++ Project1_ShaniaLi.js
@@ -0,0 +1,14 @@
+//Project 1 Shania Li Adjective: Elegant 
+function setup() {
+  createCanvas(1000, 1000);
+    background(0);
+}
+
+function draw() {
+//symbol(300,300,1);
+
+back();
+symbol(10,100,40);
+  
+  
+}
