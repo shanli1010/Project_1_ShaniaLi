8819f4a6b67c9e5e23778a3eab12c31aefb82940
shanli1010 <90589845+shanli1010@users.noreply.github.com>
2021-11-12 15:49:30 -0500

    Updated Dress
    Changed color and dress layout

--- dress.js
+++ dress.js
@@ -8,15 +8,17 @@ class Dress {
 
   display() {
  
-    push();
+    push();                          //Finaly figured out push and pop with this display function, wasn't working properly before 
     strokeWeight(2);
     translate(0,this.ypos-200);
     fill(this.c);
     
-    arc((this.xpos-50), this.ypos, 100, 100, PI, 0, CHORD); //Bust area of dress
-    arc(this.xpos+50, this.ypos, 100, 100, PI, 0, CHORD);
+    
+    arc((this.xpos-50), this.ypos, 100, 100, PI, 0, CHORD); //Bust area of dress - left
+    arc(this.xpos+50, this.ypos, 100, 100, PI, 0, CHORD); //Right bust
+    arc(this.xpos+50, this.ypos, 300, 300, ((5*PI)/4),((5*PI)/4), OPEN);
 
-    fill(132, 133, 135); //Gray Body piece of dress
+   fill(0); //Gray Body piece of dress
     noStroke();
     beginShape();
     vertex(this.xpos-100, this.ypos);
@@ -25,23 +27,67 @@ class Dress {
     vertex(this.xpos-50, this.ypos+150);
     endShape();
     
+    stroke(255);
+    line(this.xpos-100,this.ypos,this.xpos-50,this.ypos+150); //White corset lines
+    line(this.xpos-50,this.ypos,this.xpos,this.ypos+150);
+    line(this.xpos,this.ypos,this.xpos,this.ypos+150);
+     line(this.xpos+50,this.ypos,this.xpos,this.ypos+150);
+     line(this.xpos+100,this.ypos,this.xpos+50,this.ypos+150);
     
     
-    fill(26, 33, 51); //Bottom of Dress
+     fill(192,147,108);
+     noStroke();
+    rect(this.xpos-100, this.ypos+180, 200, 230); //Gold underlying part of dress
+    
+    fill(this.c); //Bottom of Dress
     strokeWeight(10);
     stroke(26, 33, 51);
-    line(this.xpos-100,this.ypos,this.xpos+50, this.ypos+150);
-    line(this.xpos+100,this.ypos,this.xpos-50, this.ypos+150);
-    line(this.xpos-100,this.ypos,this.xpos+100,this.ypos);
+   
+    
+    
     strokeWeight(13);
-    line(this.xpos,this.ypos,this.xpos+10, this.ypos-10);
+    stroke(192,147,108);
+    line(this.xpos,this.ypos,this.xpos+10, this.ypos-10); //Bow for dress
     line(this.xpos,this.ypos,this.xpos-10, this.ypos-10);
     strokeWeight(2);
     line(this.xpos,this.ypos,this.xpos+30, this.ypos+30);
     line(this.xpos,this.ypos,this.xpos-30, this.ypos+30);
-    arc(this.xpos, ((this.ypos)+300), 350, 350, PI, 0, CHORD);
+    
+    
+    noStroke();                    //White right part of flowy dress
+    beginShape();
+    curveVertex(this.xpos-65,this.ypos+130);
+    curveVertex(this.xpos-65,this.ypos+130); 
+    curveVertex(this.xpos+100, this.ypos+150);
+    curveVertex(this.xpos+120, this.ypos+450);
+    //curveVertex(this.xpos-50,this.ypos+300);
+    curveVertex(this.xpos-60, this.ypos+230);
+    curveVertex(this.xpos-65, this,ypos+130);
+    curveVertex(this.xpos-65,this.ypos+130);
+    endShape();
+    
+    fill(0);                      //Black left part of flowy dress
+    beginShape();
+    curveVertex(this.xpos+65,this.ypos+130);
+    curveVertex(this.xpos+65,this.ypos+130); 
+    curveVertex(this.xpos-100, this.ypos+150);
+    curveVertex(this.xpos-120, this.ypos+450);
+    //curveVertex(this.xpos,this.ypos+300);
+    curveVertex(this.xpos+60, this.ypos+230);
+    curveVertex(this.xpos+65, this,ypos+130);
+    curveVertex(this.xpos+65,this.ypos+130);
+    endShape();
+    
+  
+    
+    strokeWeight(20);                    //Belt
+    stroke(255);
+    line(this.xpos-55, this.ypos+130, this.xpos+55, this.ypos+130);
+   
+    
+    
     translate(0,this.ypos-150);
-    symbol(2500, 1700, 0.2);
+    symbol(2500, 1420, 0.2);          //Symbol on belt of dress
     pop();
   
 
